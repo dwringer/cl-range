6e5002740a98dd97632a9fb9c2e19d60d321afad
Darren Ringer <dwringer@gmail.com>
2017-09-21 23:54:59 -0400

    housekeeping + README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # cl-range
+This package (loosely?) emulates the xrange() function of Python 2,
+and range() of Python 3, with the crucial difference that these ranges
+are just like any other Common Lisp struct and are coerced to lists
+using the (range-to-list) function.  Slices are made (similar to
+Python's [::] slicing) using the (range-slice) and (range-subseq)
+functions, which respectively return a range and list by default.
+
+Full elaboration on the interface is not provided: if you aren't
+expecting to use this based on familiarity with the Python functions,
+I would advise against using this package unless you familiarize
+yourself with the way indexing/slicing works on Python lists.
+
+## range (start &optional stop (step 1)
+"Creates a new RANGE as described by the function parameters"
+
+### range! (start &optional stop (step 1)
+"Create a RANGE, then directly instantiate all values to a list"
+
+### range-elt (r idx)
+"Generate the element of a given RANGE designated by the given index"
+
+### range-index (r value)
+"Find, in a given RANGE, the index of a specified value"
+
+### range-reverse (r)
+"Return a RANGE that yields the same elements as a given range, in reverse"
+
+### range-to-list (r)
+"Build a list from the given RANGE"
+
+### range-slice (r &optional from to by to-list?)
+"Get the specified sub-RANGE of the given RANGE"
+
+### range-subseq (r start &optional end step)
+"Get a sequence representing the specified portion of the given RANGE"
--- range.lisp
+++ range.lisp
@@ -1,3 +1,26 @@
+;;;; RANGE.LISP - Lazy range implementation emulating Python's (x)range
+;;;; Copyright 2017 Darren W. Ringer <dwringer@gmail.com>
+
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use,
+;;;; copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;;; copies of the Software, and to permit persons to whom the
+;;;; Software is furnished to do so, subject to the following
+;;;; conditions:
+
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+;;;; OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;;;; OTHER DEALINGS IN THE SOFTWARE.
 (when (not (find-package 'deftest)) (load "deftest"))
 (defpackage :cl-range
   (:use common-lisp
@@ -26,9 +49,7 @@
     (setf start 0))
   (let ((length (multiple-value-bind (div rem)
 		    (floor (abs (- stop start)) (abs step))
-		  (if (> rem 0)
-		      (1+ div)
-		      div))))
+		  (if (> rem 0) (1+ div) div))))
     (make-range :start start :stop stop :step step :length length)))
 
 (defgeneric range-elt (r idx))
